6208e10b7287183d483f7533395bcfa958b12c9b
yalin <yalincobanoglu@yandex.com>
2018-05-21 21:41:04 +0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,37 @@
 <?php
-
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+Route::get('/', function (Request $request) {
+    return 'hello!';
+});
+
+Route::group(['prefix' => 'manage', 'namespace' => 'Admin'], function () {
+    
+    Route::get('variables/all', 'VarController@all');
+    Route::post('variables/create', 'VarController@create');
+    Route::get('variables/edit/{variable}', 'VarController@edit');
+    Route::post('variables/save/{variable}', 'VarController@save');
+    Route::delete('variables/delete/{variable}', 'VarController@delete');
+
+
+    Route::get('portfolios/all', 'PortfolioController@all');
+    Route::post('portfolios/create', 'PortfolioController@create');
+    Route::get('portfolios/edit/{variable}', 'PortfolioController@edit');
+    Route::post('portfolios/save/{variable}', 'PortfolioController@save');
+    Route::delete('portfolios/delete/{variable}', 'PortfolioController@delete');
+
+    Route::get('articles/all', function () {});
+    Route::post('articles/create', function () {});
+    Route::get('articles/edit/{id}', function () {});
+    Route::post('articles/save/{id}', function () {});
+    Route::delete('articles/delete/{id}', function () {});
+
+    Route::get('images/all', function () {});
+    Route::post('images/save', function () {});
+    Route::delete('images/delete/{id}', function () {});
+
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
