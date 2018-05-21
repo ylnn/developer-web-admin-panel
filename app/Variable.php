fe0e9496106f4f4371de3197c69efbae748daeec
yalin <yalincobanoglu@yandex.com>
2018-05-21 21:47:50 +0300

    variable global scope registered

--- app/Variable.php
+++ app/Variable.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\VariableKeyScope;
 
 class Variable extends Model
 {
-    //
+  protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new VariableKeyScope);
+    }   
 }
