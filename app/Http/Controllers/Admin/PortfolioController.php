1e31e84c19a7bdb1a142a3e6a0c23f25f7796a4d
yalin <yalincobanoglu@yandex.com>
2018-05-22 08:21:33 +0300

    resources renamed

---
+++ app/Http/Controllers/Admin/PortfolioController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Portfolio as PortfolioResource;
+use App\Http\Resources\PortfolioCollection;
+use App\Portfolio;
+use App\Http\Requests\SavePortfolio;
+
+class PortfolioController extends Controller
+{
+    public function all()
+    {
+        return new PortfolioCollection(Portfolio::all());
+    }
+
+    public function create()
+    {
+        return Portfolio::create()->id;
+    }
+
+    public function edit(Portfolio $portfolio)
+    {
+        return new PortfolioResource($portfolio);
+    }
+
+    public function save(Portfolio $portfolio, SavePortfolio $request)
+    {
+        $portfolio->title = $request->title;
+        $portfolio->description = $request->description;
+        $portfolio->url = $request->url;
+        $portfolio->save();
+        return new PortfolioResource($portfolio);
+    }
+
+    public function delete(Portfolio $portfolio)
+    {
+        $portfolio->delete();
+        return response()->json(['status' => 'success']);
+    }
+}
