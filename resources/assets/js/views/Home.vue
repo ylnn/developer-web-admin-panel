1ed3c6b9eb77721ce86f76a5438e461931a42f11
yalin <yalincobanoglu@yandex.com>
2018-05-24 08:46:22 +0300

    home.vue

---
+++ resources/assets/js/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card card-default">
+                    <div class="card-header">Home</div>
+                    <div class="card-body">Home Detail</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
