ca9d131cf5b44d6a931798b72b50a699b72f6e5e
yalin <yalincobanoglu@yandex.com>
2018-05-24 08:48:33 +0300

    modal.vue

---
+++ resources/assets/js/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+    <transition name="modal">
+    <div class="modal-mask" >
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              <button class="btn" @click="$emit('close')">
+                Close
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+    export default {
+        mounted() {
+        }
+    }
+</script>
