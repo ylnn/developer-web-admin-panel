2ae7cdb1eddb41cfe8e3522bccc65ce1a8c9b1c8
yalin <yalincobanoglu@yandex.com>
2018-05-28 14:45:41 +0300

    image portfolios relation

--- app/Image.php
+++ app/Image.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    public function portfolios()
+    {
+        return $this->belongsToMany('App\Portfolio');
+    }
 }
