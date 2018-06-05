f9ef437c74b27e9b245ce0340ae30dbeec297b4d
yalin <yalincobanoglu@yandex.com>
2018-06-05 16:24:34 +0300

    auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,47 @@
 @extends('layouts.master')
+
+
+@section('content')
+    <div id="app">
+        
+        <input type="hidden" name="token" value="{{$token}}">
+
+        <ul class="nav nav-pills" style="padding:15px;">
+            <li class="nav-item">
+                <router-link class="nav-link" tag="a" to="/" exact>
+                    Home
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" tag="a" to="/variables/index">
+                    Variables
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" tag="a" to="/portfolios/index">
+                    Portfolios
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" tag="a" to="/articles/index">
+                    Articles
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" tag="a" to="/images/index">
+                    Images
+                </router-link>
+            </li>
+
+            <li class="nav-item">
+                {{-- <a href="{{@route('logout')}}" class="nav-link">Logout</a> --}}
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" >
+                        @csrf
+                        <button class="btn btn-secondary" type="submit" class="nav-link">Logout</button>
+                    </form>
+            </li>
+        </ul>
+        {{-- Content View --}}
+        <router-view></router-view>
+    </div>
+@endsection
