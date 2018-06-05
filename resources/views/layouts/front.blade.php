9a5f9bdd0c079e9e9c612416c7e0d515ae713b89
yalin <yalincobanoglu@yandex.com>
2018-06-05 22:43:38 +0300

    title and variables

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -6,7 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/front.css')}}">
-    <title>Title</title>
+    @isset($title)
+        <title>{{$title}}</title>
+    @else 
+        <title>{{$variables->where('key', 'site-title')->first()->value ?? ''}}</title>
+    @endif
 
 </head>
 <body>
@@ -22,11 +26,21 @@
                 </div>
                 <div class="col-4 d-flex justify-content-end">
                     <div class="header-right">
-                        <img width="30" src="/images/mail.svg" alt="Mail">
-                        <img width="30" src="/images/behance.svg" alt="Behance">
-                        <img width="30" src="/images/dribbble.svg" alt="Dribbble">
-                        <img width="30" src="/images/linkedin.svg" alt="Linkedin">
-                        <img width="30" src="/images/twitter.svg" alt="Twitter">
+                        <a href="{{$variables->where('key', 'email')->first()->value ?? ''}}">
+                                <img width="30" src="/images/mail.svg" alt="Mail">
+                        </a>
+                        <a href="{{$variables->where('key', 'behance')->first()->value ?? ''}}">
+                            <img width="30" src="/images/behance.svg" alt="Behance">
+                        </a>
+                        <a href="{{$variables->where('key', 'dribbble')->first()->value ?? ''}}">
+                            <img width="30" src="/images/dribbble.svg" alt="Dribbble">
+                        </a>
+                        <a href="{{$variables->where('key', 'linkedin')->first()->value ?? ''}}">
+                            <img width="30" src="/images/linkedin.svg" alt="Linkedin">
+                        </a>
+                        <a href="{{$variables->where('key', 'twitter')->first()->value ?? ''}}">
+                            <img width="30" src="/images/twitter.svg" alt="Twitter">
+                        </a>
                     </div>
                 </div>
             </div>
@@ -37,7 +51,7 @@
         </div>
 
         <div class="footer d-flex justify-content-center">
-            <div>(c) 2018 | ugurbolge.com | dribble | email</div>
+            <div>{{$variables->where('key', 'footer-text')->first()->value ?? ''}}</div>
         </div>
 
     </div>
