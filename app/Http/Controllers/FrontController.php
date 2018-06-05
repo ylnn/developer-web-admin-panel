2455032040cd9549353e6d52999a9e6a3314f5a3
yalin <yalincobanoglu@yandex.com>
2018-06-05 23:28:18 +0300

    image replace

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -18,6 +18,31 @@ class FrontController extends Controller
     public function article(Article $article)
     {
         $variables = Variable::all();
-        return view('front.article', compact('article', 'variables'));
+
+
+        $image_size = $variables->where('key', 'image-size')->first()->value ?? '';
+        
+        if(!$image_size){
+           abort(500, 'image size error');
+        }
+
+       if(!preg_match('/^(\d+)x(\d+)$/', $image_size)){
+           abort(500, 'image size error');
+       }
+
+       $expSize = explode('x', $image_size);
+
+       $image_size = str_replace('x', '/', $image_size);
+
+        $images = $article->images->pluck('filename')->map(function ($filename) use ($image_size){
+            return "<img src=\"../image/$image_size/$filename\">";
+        });
+
+
+        $content = $article->description;
+
+        $replaced = preg_replace_array('/(\[image(|.*?)(|.*?)\])/', $images->toArray(), $content);
+        
+        return view('front.article', compact('article', 'variables', 'replaced'));
     }
 }
