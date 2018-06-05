b9ace0d0b162108352503c5912c679916ec69c6a
yalin <yalincobanoglu@yandex.com>
2018-06-05 21:57:48 +0300

    variables

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use App\Variable;
 
 class FrontController extends Controller
 {
     public function index()
     {
         $articles = Article::latest()->get();
-        return view('front.home', compact('articles'));
+        $variables = Variable::all();
+        return view('front.home', compact('articles', 'variables'));
     }
+
     public function article(Article $article)
     {
-        return view('front.article', compact('article'));
+        $variables = Variable::all();
+        return view('front.article', compact('article', 'variables'));
     }
 }
