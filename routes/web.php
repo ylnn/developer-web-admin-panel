c44c28f5456e2a37c5782c6827834209be4a101c
yalin <yalincobanoglu@yandex.com>
2018-06-05 16:26:56 +0300

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Mainpage';
 });
 
+
+Route::get('/manage', 'ManageController@index');
 
 Route::get('/image/{h}/{w}/{filename}','ImageCacheController@show')
 ->where('h', '[0-9]+')
 ->where('w', '[0-9]+')
 ->where('filename', '[a-zA-Z0-9.]+')
 ;
+
+// Auth::routes();
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
