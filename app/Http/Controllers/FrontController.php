864d9e4a63314038925721ca34c4c71a40581d4a
yalin <yalincobanoglu@yandex.com>
2018-06-06 13:35:43 +0300

    image map refactor

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Article;
 use App\Variable;
+use App\Http\Repositories\ImageRepository;
 
 class FrontController extends Controller
 {
@@ -15,52 +16,12 @@ class FrontController extends Controller
         return view('front.home', compact('articles', 'variables'));
     }
 
-    public function article(Article $article)
+    public function article(Article $article, ImageRepository $repo)
     {
         $variables = Variable::all();
 
-        $image_size = $variables->where('key', 'image-size')->first()->value ?? '';
-        
-        if(!$image_size){
-           abort(500, 'image size error');
-        }
+        $replaced = $repo->mapImages($variables, $article);
 
-       if(!preg_match('/^(\d+)x(\d+)$/', $image_size)){
-           abort(500, 'image size error');
-       }
-
-       $expSize = explode('x', $image_size);
-
-       $image_size = str_replace('x', '/', $image_size);
-
-        $images = $article->images->pluck('filename')->map(function ($filename) use ($image_size){
-            return "<img src=\"../image/$image_size/$filename\">";
-        });
-
-        $new_images = $article->images;
-
-        $content = $article->description;
-
-        $i = 0;
-
-        $newContent = preg_replace_callback("/\[image\|?(.*?)\]/", function($matches) use ($new_images, $image_size) {
-            
-            global $i;
-            $i++;
-
-            if(isset($new_images[$i-1])) {
-                $filename =  $new_images[$i-1]->filename;
-                $alt = $matches[1] ?? '';
-                $image_url = url('image');
-                return "<img src=\"$image_url/$image_size/$filename\" alt=\"$alt\">";
-            }
-            return '!!! IMAGE NOT FOUND !!!';
-            
-            
-        }, $content);
-
-        $replaced = preg_replace_array('/(\[image(|.*?)(|.*?)\])/', $images->toArray(), $newContent);
-        
         return view('front.article', compact('article', 'variables', 'replaced'));
     }
 }
---
+++ app/Http/Repositories/ImageRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Article;
+
+class ImageRepository 
+{
+    public $variables;
+    public $article;
+    public $imageSize;
+    public $newContent;
+
+
+    public function mapImages($variables, Article $article)
+    {
+        $this->variables = $variables;
+        $this->article = $article;
+
+        $this->getImageSize();
+
+        $this->checkImageSizeExists();
+
+        $this->checkImageSize();
+
+        $this->transformImageSize();
+
+        $this->replaceImages();
+
+        return $this->newContent;
+
+    }
+   
+    public function getImageSize()
+    {
+        $this->imageSize = $this->variables->where('key', 'image-size')->first()->value ?? '';
+    }
+
+    public function checkImageSizeExists()
+    {
+        if(!$this->imageSize){
+            abort(500, 'image size error');
+        }   
+    }
+
+    public function checkImageSize()
+    {
+        if(!preg_match('/^(\d+)x(\d+)$/', $this->imageSize)){
+            abort(500, 'image size error');
+        }
+    }
+
+
+    public function transformImageSize()
+    {
+        $expSize = explode('x', $this->imageSize);
+        $this->imageSize = str_replace('x', '/', $this->imageSize);   
+    }
+
+    public function replaceImages($i = 0)
+    {
+        $articleImages = $this->article->images;
+        $imageSize = $this->imageSize;
+
+        $this->newContent = preg_replace_callback("/\[image\|?(.*?)\]/", function($matches) use ($articleImages, $imageSize) {
+            global $i;
+            $i++;
+            if(isset($articleImages[$i-1])) {
+                $filename =  $articleImages[$i-1]->filename;
+                $alt = $matches[1] ?? '';
+                $image_url = url('image');
+                return "<img src=\"$image_url/$imageSize/$filename\" alt=\"$alt\">";
+            }
+            return '!!! IMAGE NOT FOUND !!!';
+        }, $this->article->description);   
+    }
+}
