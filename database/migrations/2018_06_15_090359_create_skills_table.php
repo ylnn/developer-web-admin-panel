d3cdf84eeab17b72767abd56b185025dfa1b49b8
yalin <yalincobanoglu@yandex.com>
2018-06-15 12:42:31 +0300

    skills migrate & model

---
+++ database/migrations/2018_06_15_090359_create_skills_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order')->default(0);
+            $table->string('skill')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+}
