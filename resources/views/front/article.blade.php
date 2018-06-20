3bb02badafdafcbaf19e1a380c963fb61ff7e8c0
yalin <yalincobanoglu@yandex.com>
2018-06-20 19:22:24 +0300

    give notify if record is draft

--- resources/views/front/article.blade.php
+++ resources/views/front/article.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div class="article">
             <div class="title">{{$article->title}}</div>
+            @if($article->status == "draft")
+                <div class="badge badge-warning">DRAFT</div>
+            @endif
             <div class="date">{{$article->created_at->format('l, d F Y')}}</div>
             <div class="summary">{!! $replaced !!}</div>
         </div>
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -6,6 +6,9 @@
         @foreach ($articles as $article)
             <div class="article">
                 <div class="title">{{$article->title}}</div>
+                @if($article->status == "draft")
+                    <div class="badge badge-warning">DRAFT</div>
+                @endif
                 <div class="date">{{$article->created_at->format('l, d F Y')}}</div>
                 <div class="summary">{{$article->summary}}</div>
                 <div class="read-more">
