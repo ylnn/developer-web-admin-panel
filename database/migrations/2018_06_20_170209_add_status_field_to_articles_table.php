1668ed0b96aef15a0f39c51c6dd0025084f3e349
yalin <yalincobanoglu@yandex.com>
2018-06-20 20:02:59 +0300

    status field moved to new migration

--- database/migrations/2018_06_20_170209_add_status_field_to_articles_table.php
+++ database/migrations/2018_06_20_170209_add_status_field_to_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticlesTable extends Migration
+class AddStatusFieldToArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title')->nullable();
-            $table->text('description')->nullable();
-            $table->string('url')->nullable();
-            $table->timestamps();
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('status')->index()->default('draft');
         });
     }
 
@@ -29,6 +25,8 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
     }
 }
