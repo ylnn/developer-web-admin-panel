28ded3849a7c5da499fcf4fc7fb76fe847c8dd80
yalin <yalincobanoglu@yandex.com>
2018-06-20 19:32:02 +0300

    normal image show route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Route::get('/image/{h}/{w}/{filename}','ImageCacheController@show')
 ->where('w', '[0-9]+')
 ->where('filename', '[a-zA-Z0-9.]+');
 
+Route::get('/image/{filename}','ImageCacheController@showNormalImage')
+->where('filename', '[a-zA-Z0-9.]+');
+
 // Auth::routes();
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login');
