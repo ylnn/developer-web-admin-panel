83022f239cf1f0336f7cc5bfb226fa7054e4eb40
yalin <yalincobanoglu@yandex.com>
2018-06-28 11:29:49 +0300

    excerpt added to front files

--- resources/views/front/article.blade.php
+++ resources/views/front/article.blade.php
@@ -7,6 +7,7 @@
                 <div class="badge badge-warning">DRAFT</div>
             @endif
             <div class="date">{{$article->created_at->format('l, d F Y')}}</div>
+            <div class="excerpt">{{ $article->excerpt }}</div>
             <div class="summary">{!! $replaced !!}</div>
         </div>
 @endsection
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -10,7 +10,7 @@
                     <div class="badge badge-warning">DRAFT</div>
                 @endif
                 <div class="date">{{$article->created_at->format('l, d F Y')}}</div>
-                <div class="summary">{{$article->summary}}</div>
+                <div class="summary">{{$article->excerpt}}</div>
                 <div class="read-more">
                     <a href="{{ route('article.detail', [$article->id, $article->url]) }}" class="btn btn-dark">Read more...</a>
                 </div>
